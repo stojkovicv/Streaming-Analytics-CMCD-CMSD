dbf6c216676321b8019207b97384efc41be23310
root <vukbg.stojkovic@gmail.com>
2023-03-10 19:29:07 +0100

    testing with POST

--- load-test/loadtesting.sh
+++
@@ -1,5 +0,0 @@
-# cmcd-server
-locust -f locust.py --host http://localhost:5500/stream-client/ --users 10000 --spawn-rate 5000
-
-# cmcd-bridge
-# locust -f locust.py --host http://localhost:8088 --users 100 --spawn-rate 80
--- load-test/locust.py
+++ load-test/locust.py
@@ -1,8 +1,25 @@
 from locust import HttpUser, task, between
 
-class StreamingUser(HttpUser):
+class CMCDUser(HttpUser):
     wait_time = between(1, 5)
 
     @task
-    def cmcd_server(self):
-        self.client.get("http://localhost:5500/stream-client/", stream=True)
+    def send_cmcd_logs(self):
+        params = {
+            "sf": "d",
+            "time": "2023-02-14T14:26:06+00:00",
+            "d": "4000",
+            "st": "v",
+            "rtp": "5000",
+            "br": "4952",
+            "nor": "bbb_30fps_1280x720_4000k_8.m4v",
+            "bl": "20200",
+            "cid": "21cf726cfe3d937b5f974f72bb5bd06a",
+            "sid": "3a799169-87c9-40bd-bc8f-cf7aacdb2a79",
+            "mtp": "587100",
+            "dl": "20200",
+            "ot": "v",
+            "bs": "true",
+            "su": "false"
+            }
+        self.client.post('/cmcd-bridge', json=params)
