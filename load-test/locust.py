53e879ee33adfda761bf919a43a9baa28148c8c3
root <vukbg.stojkovic@gmail.com>
2023-03-10 22:08:40 +0100

    modified URL

--- load-test/locust.py
+++ load-test/locust.py
@@ -22,4 +22,4 @@ class CMCDUser(HttpUser):
             "bs": "true",
             "su": "false"
             }
-        self.client.post('/cmcd-bridge', json=params)
+        self.client.post('/cmcd/media/vod/bbb_30fps_akamai/bbb_30fps_320x180_400k/bbb_30fps_320x180_400k_0.m4v?CMCD=cid%3D%2221cf726cfe3d937b5f974f72bb5bd06a%22%2Cot%3Di%2Csf%3Dd%2Csid%3D%225a16d9dd-ca72-4c0c-9eb9-b013815060bb%22%2Cst%3Dv%2Csu', json=params)
